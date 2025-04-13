8952edc92a7055cc49ac5d79818aaa734dc902e0
artificialair <jaybaker964@gmail.com>
2025-04-13 08:31:31 -0400

    main dot dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -9,9 +9,8 @@ import 'httphandling.dart';
 
 // --------------------------------------------------------------------------------
 
-
 void main() {
-    runApp(MaterialApp(home: TranslatorAppImplementation()));
+  runApp(MaterialApp(home: TranslatorAppImplementation()));
 }
 
 class TranslatorApp extends StatelessWidget {
@@ -22,28 +21,30 @@ class TranslatorApp extends StatelessWidget {
     return const MaterialApp(home: TranslatorAppImplementation());
   }
 }
+
 class tLCD {
   static Map<String, String> languageCodes = {
-  'English': 'en',
-  'Spanish': 'es',
-  'Japanese': 'ja',
-  'Latin': 'la',
-  'French': 'fr',
-  'German': 'de',
-  'Korean': 'ko',
-  'Swedish': 'sv',
-  'Russian': 'ru',
-  'Norwegian': 'no',
-  'Arabic': "ar",
-};
+    'English': 'en',
+    'Spanish': 'es',
+    'Japanese': 'ja',
+    'Latin': 'la',
+    'French': 'fr',
+    'German': 'de',
+    'Korean': 'ko',
+    'Swedish': 'sv',
+    'Russian': 'ru',
+    'Norwegian': 'no',
+    'Arabic': "ar",
+  };
   static final List<MenuEntry> menuEntries = UnmodifiableListView<MenuEntry>(
-  languageCodes.keys.map<MenuEntry>((String name) => MenuEntry(value: name, label: name)),
-);
+    languageCodes.keys.map<MenuEntry>(
+      (String name) => MenuEntry(value: name, label: name),
+    ),
+  );
 
   static String dropdownValuesrc = languageCodes.keys.first;
   static String dropdownValuedest = languageCodes.keys.first;
 }
-
 
 class HTTPNotifier extends ChangeNotifier {
   String src_code = "en";
@@ -51,9 +52,10 @@ class HTTPNotifier extends ChangeNotifier {
   String substr = "";
   late var http_endpoint;
   final http_url = "https://translator-backend-kbqg.onrender.com";
-  
+
   void updateURL(String str, String src, String dest) {
-    http_endpoint = "${http_url}/translate?string=${str}&src=${src}&dest=${dest}";
+    http_endpoint =
+        "${http_url}/translate?string=${str}&src=${src}&dest=${dest}";
     if (src != src_code) {
       src_code = src;
     }
@@ -69,7 +71,7 @@ class HTTPNotifier extends ChangeNotifier {
 
 class TextNotifier extends ChangeNotifier {
   String translated_text = "";
-  
+
   void updateText(String text) {
     translated_text = text;
     notifyListeners();
@@ -94,91 +96,160 @@ class _TranslatorAppState extends State<TranslatorAppImplementation> {
   TextNotifier _textNotifier = new TextNotifier();
   HTTPNotifier _httpNotifier = new HTTPNotifier();
 
-    
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.sizeOf(context);
 
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(25),
+        padding: EdgeInsets.symmetric(vertical: 25),
         alignment: FractionalOffset.center,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(vertical: size.height*0.1, horizontal: size.width*0.1),
+              padding: EdgeInsets.symmetric(
+                vertical: size.height * 0.1,
+                horizontal: size.width * 0.1,
+              ),
               child: Column(
-                children: <Widget> [
+                children: <Widget>[
                   Padding(
                     padding: EdgeInsets.symmetric(vertical: 15),
                     child: SizedBox(
-                    width: size.width*0.2,
-                    height: size.height*0.4,
-                    child: TextField(
-                      controller: _controller,
-                      minLines: 25,
-                      maxLines: 25,
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.done,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(), 
-                        labelText: 'Input text to be translated',), 
-                      onSubmitted: (String value) async {
-                        _httpNotifier.updateURL(value, _httpNotifier.src_code, _httpNotifier.dest_code);
-                        print(_httpNotifier.http_endpoint);
-                        var response = await http.get(Uri.parse(_httpNotifier.http_endpoint));
-                        if (response.statusCode == 200) {
-                          var translated_text = jsonDecode(response.body)["content"];
-                          print(translated_text);
-                          _textNotifier.updateText(translated_text);
-                        }
-                        else {
-                          print("error with the http stuff, status code: ${response.statusCode}, data: ${response.body}");
-                        }
-                      },
-                    ),
-                  ),
-                ),
-                
-                  Container(
-                    child: ListenableBuilder(
-                      listenable: _httpNotifier,
-                      builder: (context, child) {
-                        return LanguageDropdown(notifier: _httpNotifier, identifier: 1);
-                      }
-                    ),
-                  ),
-                  ],
-                ),
-              ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: size.height*0.1, horizontal: size.width*0.1),
-              child: Column (
-                children: <Widget> [
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical:15),
-                    child: SizedBox(
-                    width: size.width*0.15,
-                    height: size.height*0.4,
-                    child: Container(
-                      decoration: BoxDecoration(border: Border.all()),
-                      child: ListenableBuilder(
-                        listenable: _textNotifier,
-                        builder: (context, child) {
-                          return Text("Translation: ${_textNotifier.translated_text}");
-                        }
+                      width: size.width * 0.2,
+                      height: size.height * 0.4,
+                      child: TextField(
+                        controller: _controller,
+                        minLines: 25,
+                        maxLines: 25,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.done,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Input text to be translated',
+                        ),
+                        onSubmitted: (String value) async {
+                          _httpNotifier.updateURL(
+                            value,
+                            _httpNotifier.src_code,
+                            _httpNotifier.dest_code,
+                          );
+                          print(_httpNotifier.http_endpoint);
+                          var response = await http.get(
+                            Uri.parse(_httpNotifier.http_endpoint),
+                          );
+                          if (response.statusCode == 200) {
+                            var translated_text =
+                                jsonDecode(response.body)["content"];
+                            print(translated_text);
+                            _textNotifier.updateText(translated_text);
+                          } else {
+                            print(
+                              "error with the http stuff, status code: ${response.statusCode}, data: ${response.body}",
+                            );
+                          }
+                        },
                       ),
-                    )
+                    ),
+                  ),
+
+                  ListenableBuilder(
+                    listenable: _httpNotifier,
+                    builder: (context, child) {
+                      return LanguageDropdown(
+                        notifier: _httpNotifier,
+                        identifier: 1,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    String text = _controller.text;
+                    _httpNotifier.updateURL(
+                      text,
+                      _httpNotifier.src_code,
+                      _httpNotifier.dest_code,
+                    );
+                    var response = await http.get(
+                      Uri.parse(_httpNotifier.http_endpoint),
+                    );
+                    if (response.statusCode == 200) {
+                      var translatedText = jsonDecode(response.body)["content"];
+                      _textNotifier.updateText(translatedText);
+                    } else {
+                      _textNotifier.updateText(
+                        "HTTP Error, please try again later.",
+                      );
+                    }
+                  },
+                  child: Text('Translate!'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.mic_rounded,
+                    color: _recording ? Colors.black : Colors.red,
+                    size: 60,
                   ),
                 ),
-                  Container(
-                    child: ListenableBuilder(
-                      listenable: _httpNotifier,
-                      builder: (context, child) {
-                        return LanguageDropdown(notifier: _httpNotifier, identifier: 2);
-                      },
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      // Toggle light when tapped.
+                      _recording = !_recording;
+                    });
+                  },
+                  child: Container(
+                    color: Colors.yellow.shade600,
+                    padding: const EdgeInsets.all(8),
+                    // Change button text when light changes state.
+                    child: Text(
+                      _recording ? 'Begin Recording' : 'End Recording',
                     ),
+                  ),
+                ),
+              ],
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: size.height * 0.1,
+                horizontal: size.width * 0.1,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: SizedBox(
+                      width: size.width * 0.15,
+                      height: size.height * 0.4,
+                      child: Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        child: ListenableBuilder(
+                          listenable: _textNotifier,
+                          builder: (context, child) {
+                            return Text(
+                              "Translation: ${_textNotifier.translated_text}",
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  ListenableBuilder(
+                    listenable: _httpNotifier,
+                    builder: (context, child) {
+                      return LanguageDropdown(
+                        notifier: _httpNotifier,
+                        identifier: 2,
+                      );
+                    },
                   ),
                 ],
               ),
@@ -194,55 +265,41 @@ class LanguageDropdown extends StatefulWidget {
   HTTPNotifier notifier;
   int identifier;
 
-  LanguageDropdown({Key? key, required this.notifier, required this.identifier}) : super(key: key);
+  LanguageDropdown({Key? key, required this.notifier, required this.identifier})
+    : super(key: key);
 
+  @override
   State<LanguageDropdown> createState() => _LanguageDropdownState();
 }
 
 class _LanguageDropdownState extends State<LanguageDropdown> {
-  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListenableBuilder(
-        listenable: widget.notifier,
-        builder: (context, child) {
-          return DropdownMenu<String>(
+    return ListenableBuilder(
+      listenable: widget.notifier,
+      builder: (context, child) {
+        return DropdownMenu<String>(
           initialSelection: tLCD.languageCodes.keys.first,
           onSelected: (String? value) {
             setState(() {
-                (widget.identifier == 1) ? tLCD.dropdownValuesrc = value! : tLCD.dropdownValuedest = value!;
-              });
-              String localLanguageCodedest = tLCD.languageCodes[tLCD.dropdownValuedest]!;
-              String localLanguageCodesrc = tLCD.languageCodes[tLCD.dropdownValuesrc]!;
-              widget.notifier.updateURL(widget.notifier.substr, localLanguageCodesrc, localLanguageCodedest);
-              print(widget.notifier.http_endpoint);
-            },
+              (widget.identifier == 1)
+                  ? tLCD.dropdownValuesrc = value!
+                  : tLCD.dropdownValuedest = value!;
+            });
+            String localLanguageCodedest =
+                tLCD.languageCodes[tLCD.dropdownValuedest]!;
+            String localLanguageCodesrc =
+                tLCD.languageCodes[tLCD.dropdownValuesrc]!;
+            widget.notifier.updateURL(
+              widget.notifier.substr,
+              localLanguageCodesrc,
+              localLanguageCodedest,
+            );
+            print(widget.notifier.http_endpoint);
+          },
           dropdownMenuEntries: tLCD.menuEntries,
-          );
-        }
-      ),
+        );
+      },
     );
   }
 }
-/*Padding( padding: const EdgeInsets.all(8.0),
-child: Icon(
-Icons.mic_rounded,
-color: _recording ? Colors.black: Colors.red,
-size: 60,
-),
-),
-GestureDetector(
-onTap: () {
-setState(() {
-// Toggle light when tapped.
-_recording = !_recording;
-});
-},
-child: Container(
-color: Colors.yellow.shade600,
-padding: const EdgeInsets.all(8),
-// Change button text when light changes state.
-child: Text(_recording ? 'Begin Recording' : 'End Recording'),
-),
-),*/

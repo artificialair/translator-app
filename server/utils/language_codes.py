cecab4e846a911e604c39b8ebb36869b7da56383
artificialair <jaybaker964@gmail.com>
2025-04-13 05:46:12 -0400

    added support for more languages

--- server/utils/language_codes.py
+++ server/utils/language_codes.py
@@ -1,4 +1,17 @@
 codes = {
     "en": "en-US",
-    "es": "es-US"
+    "es": "es-US",
+    "fr": "fr-FR",
+    "it": "it-IT",
+    "de": "de-DE",
+    "nl": "nl-NL",
+    "pt": "pt-BR",
+    "sv": "sv-SE",
+    "no": "no-NO",
+    "ar": "ar-IL",
+    "ru": "ru-RU",
+    "ja": "ja-JP",
+    "zh-cn": "cmn-Hans-CN",
+    "zh-tw": "cmn-Hant-TW",
+    "ko": "ko-KR",
 }
